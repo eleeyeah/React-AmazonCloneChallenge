ba8f9873904deae1ecfc2a33d29ec7437b064069
eleeyeah <ilietmks@gmail.com>
2022-09-01 20:25:52 +0200

    stateProvider & reducer

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,32 @@
 export const initialState = {
     basket: [],
-    
+
 };
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TO_BASKET:
+            return {
+                ...state,
+                basket: [...state.basket, action.item],
+            };
+        /* case REMOVE_FROM_BASKET:
+            const newBasket = [...state.basket];
+            const index = state.basket.findIndex(
+                basketItem => basketItem.id === action.id
+            );
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+            } else {
+                console.warn(
+                    `Can't remove product (id: ${action.id}) as it's not in the basket!`
+                );
+            }
+            return { ...state, basket: newBasket };
+        default:
+            return state;
+    } */
+    }
+};
+
+export default reducer;
